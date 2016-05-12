ff5bfe3f3eb811ea585c1a5dad2ff1ab0e57c3bc
ouxss <ouxssxuo@gmail.com>
2016-05-12 15:42:58 -0500

    installation file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+apt-get install python-pip python-dev build-essential 
+pip install --upgrade pip 
+pip install --upgrade virtualenv 
+pip install termcolor
+pip install configparser
+pip install scapy
+pip install konsole
+pip install pygeoip
+pip install dpkt
+pip install ipaddr
+echo 'Done.'
